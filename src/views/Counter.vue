2b3976ac830f568e77048f17d8cdd08f75340878
Alif <alifdelgado@gmail.com>
2022-01-26 10:58:48 -0600

    First commit

---
+++ src/views/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Counter Vue</h1>
+  <h2>{{ counter }}</h2>
+  <button @click="increase">+1</button>
+  <button @click="decrease">-1</button>
+</template>
+
+<script>
+import useCounter from '@/composables/useCounter';
+
+export default {
+    name: 'Counter',
+    props: {},
+    emits: [],
+    setup(props) {
+        const { counter, increase, decrease } = useCounter()
+        return { counter, increase, decrease };
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>{{ ironman }}</h3>
+    <h3>{{ hulk }}</h3>
+    <button @click="changeIronman">Change ironman</button>
+    <button @click="changeHulk">Change hulk</button>
+    <h3>Ironman: {{ironmanIsAlive}}</h3>
+    <h3>HulkIronman: {{hulkIsAlive}}</h3>
+    <button @click="changeStatus">Cambiar status</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    const ironman = ref({ name: 'Tony', age: 50 });
+    const hulk = reactive({ name: 'Bruce', age: 50 });
+    let ironmanIsAlive = reactive(false);
+    const hulkIsAlive = ref(true);
+
+    const changeStatus = () => {
+      hulkIsAlive.value = !hulkIsAlive.value;
+      ironmanIsAlive != ironmanIsAlive;
+    };
+
+    return {
+      ironman,
+      hulk,
+      ironmanIsAlive,
+      hulkIsAlive,
+      changeStatus,
+      changeIronman: () => {
+        ironman.value.name = 'Tony Stark'
+        ironman.value.age = 55
+      },
+      changeHulk: () => {
+        hulk.name = 'Bruce Banner'
+        hulk.age = 55
+      }
+    }
   }
 }
 </script>
